78b061a6bf00ebea622ea77e4177c5a11a38a589
Sola Kabuta <kam@MacBook-Pro-de-Sola.local>
2025-02-19 12:29:21 +0100

    fix: frontal controller fixed

--- public/index.php
+++ public/index.php
@@ -6,8 +6,31 @@ if (isset($_GET['p'])) {
     switch($_GET['p']) {
         case 'geographie':
             $title = 'geographie';
-            include '../templates/geographie';
+            include '../templates/geographie.php';
             break;
+        case 'histoire':
+            $title = 'histoire';
+            include '../templates/histoire.php';
+            break;
+        case 'culture':
+            $title = 'culture';
+            include '../templates/culture.php';
+            break;
+        case 'galerie':
+            $title = 'galerie';
+            include '../templates/galerie.php';
+            break;
+        case 'contact':
+            $title = 'contact';
+            include '../templates/contact.php';
+            break;
+        case 'liens':
+            $title = 'liens';
+            include '../templates/liens.php';
+            break;
+        default:
+            $title = 'page-404';
+            include '../templates/page-404.php';
     }
 } else {
     $title = 'accueil';
