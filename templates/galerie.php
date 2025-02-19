799ecaec5aa029dc4b0a5f492f72e42e08ebdcf1
Sola Kabuta <kam@MacBook-Pro-de-Sola.local>
2025-02-19 13:39:27 +0100

    style: gallery img size changed

--- templates/galerie.php
+++ templates/galerie.php
@@ -35,28 +35,28 @@
                 <p>Marcus Aurelius</p>
             </div>
             <div class="card">
-                <img src="img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="marcus aurelius statue pic">
-                <p>Marcus Aurelius</p>
+                <img src="img/480px_Fontana_de_Trevi.jpg" alt="Fontana de Trevi">
+                <p>Fontana de Trevi</p>
             </div>
         </div>
         <div class="gallery">
             <div class="card">
-                <img src="img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="marcus aurelius statue pic">
-                <p>Marcus Aurelius</p>
+                <img src="img/480px_Pantheon_Rome.jpg" alt="Pantheon">
+                <p>Pantheon</p>
             </div>
             <div class="card">
-                <img src="img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="marcus aurelius statue pic">
-                <p>Marcus Aurelius</p>
+                <img src="img/480px_Piazza_Navona.jpg" alt="Piazza Navona">
+                <p>Piazza Navona</p>
             </div>
         </div>
         <div class="gallery">
             <div class="card">
-                <img src="img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="marcus aurelius statue pic">
-                <p>Marcus Aurelius</p>
+                <img src="img/480px_Pont_Saint-Ange_et_le_Vatican.jpg" alt="Pont Saint Ange pic">
+                <p>Saint Ange & Vatican</p>
             </div>
             <div class="card">
-                <img src="img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="marcus aurelius statue pic">
-                <p>Marcus Aurelius</p>
+                <img src="img/480px_Quirinale_palazzo_e_obelisco.jpg" alt="Palazzo pic">
+                <p>Palazzo</p>
             </div>
         </div>
 </body>
