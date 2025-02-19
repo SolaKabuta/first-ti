6e9df91e31018d36cc91c6b0f2df5f1e7e781e5f
Sola Kabuta <kam@MacBook-Pro-de-Sola.local>
2025-02-19 11:38:37 +0100

    style: home page layout improved

--- templates/inc/banniere.php
+++ templates/inc/banniere.php
@@ -1,1 +1,4 @@
-<img class='banner' src="../public/img/banniere.jpg" alt="bannière du header">
+<div class="banner-container">
+    <img class='banner' src="../public/img/banniere.jpg" alt="bannière du header">
+    <h1 class="title">Rome</h1>
+</div>
--- templates/inc/menu.php
+++ templates/inc/menu.php
@@ -1,9 +1,11 @@
-<ul>
-    <li><a href="./">Accueil</a></li>
-    <li><a href="?p=geographie?">Géographie</a></li>
-    <li><a href="?p=histoire?">Histoire</a></li>
-    <li><a href="?p=culture">Culture</a></li>
-    <li><a href="?p=galerie?">Galerie</a></li>
-    <li><a href="?p=contact?">Contact</a></li>
-    <li><a href="?p=liens?">Liens</a></li>
-</ul>
+<nav>
+    <ul>
+        <li><a href="./">Accueil</a></li>
+        <li><a href="?p=geographie?">Géographie</a></li>
+        <li><a href="?p=histoire?">Histoire</a></li>
+        <li><a href="?p=culture">Culture</a></li>
+        <li><a href="?p=galerie?">Galerie</a></li>
+        <li><a href="?p=contact?">Contact</a></li>
+        <li><a href="?p=liens?">Liens</a></li>
+    </ul>
+</nav>
